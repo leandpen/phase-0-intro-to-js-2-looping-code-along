10ffb2f0b7a51915c0e9dbde9648e751af221725
leandro <romeroleandro0711@gmail.com>
2023-03-10 17:23:07 -0500

    complete lab

--- index.js
+++ index.js
@@ -7,17 +7,16 @@ function countDown(number) {
  return number;
 }
 
-
-wrapGifts(gifts);
-
-let names = ["Guadalupe", "Ollie", "Aki",];
+const gifts = ["Guadalupe ", "Ollie ", "Aki "]
 
 function writeCards(names, event) {
     let messages = []
     for (let i = 0; i < names.length; i++) {
-      messages.push("Thank you, " + names[i] + " for the wonderful " + event + " gift!")
+      messages.push ("Thank you, " + names[i] + ", for the wonderful "  + event + " gift!");
     }
     return messages;
   }
+
+
   console.log ["Guadalupe", "Ollie", "Aki"];
-console.log(writeCards(names, enent));
+console.log(writeCards(names));
