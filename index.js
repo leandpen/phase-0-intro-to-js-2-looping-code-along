52a1a5f74cd95debf9e96f786090ef019091caf3
leandro <romeroleandro0711@gmail.com>
2023-03-10 03:22:15 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+function countDown(number) {
+ let x = number
+ while (x >= 0) { 
+    console.log(x);
+    x--;
+ }
+ return number;
+}
+
+
+wrapGifts(gifts);
+
+let names = ["Guadalupe", "Ollie", "Aki",];
+
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+      messages.push("Thank you, " + names[i] + " for the wonderful " + event + " gift!")
+    }
+    return messages;
+  }
+  console.log ["Guadalupe", "Ollie", "Aki"];
+console.log(writeCards(names, enent));
